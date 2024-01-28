2784bb13dd1f242a503c7f35b5b3d60fe9f62f73
lucas-faget <lucas.faget@laposte.net>
2024-01-28 23:51:50 +0100

    First commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+    import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+    <header>
+        <nav>
+            <RouterLink to="/">Home</RouterLink>
+            <RouterLink to="/room">Room</RouterLink>
+        </nav>
+    </header>
+
+    <RouterView />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/RoomView.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+    // const connection = new signalR.HubConnectionBuilder()
+    //     .withUrl("/quizHub")
+    //     .configureLogging(signalR.LogLevel.Information)
+    //     .build();
+
+    // async function start() {
+    //     try {
+    //         await connection.start();
+    //         console.log("SignalR Connected.");
+    //     } catch (err) {
+    //         console.log(err);
+    //         setTimeout(start, 5000);
+    //     }
+    // };
+
+    // connection.onclose(async () => {
+    //     await start();
+    // });
+
+    // // Start the connection.
+    // start();
+</script>
+
+<template>
+    <main>
+        Room
+    </main>
+</template>
