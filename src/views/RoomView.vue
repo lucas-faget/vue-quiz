7c8fef39eed12c3a17a96e97db2a1b30895c345a
lucas-faget <lucas.faget@laposte.net>
2024-01-30 17:32:41 +0100

    Create call client method to get room questions, Display chat and questions in the view

--- src/views/RoomView.vue
+++ src/views/RoomView.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts">
+    import { ref } from 'vue';
     import * as signalR from "@microsoft/signalr";
+
+    const chat = ref([]);
+    const quiz = ref([]);
 
     const connection = new signalR.HubConnectionBuilder()
         // .withUrl("wss://localhost:7052/quizHub")
@@ -17,6 +21,7 @@
 
             try {
                 await connection.invoke("JoinRoom", 0, "");
+                await connection.invoke("GetQuestions", 0);
             } catch (err) {
                 console.error("Failed to join room:", err);
             }
@@ -25,9 +30,15 @@
             setTimeout(start, 5000);
         }
     };
-
+    
     connection.on("ReceiveMessage", (message) => {
         console.log(message);
+        chat.value.push(message);
+    });
+    
+    connection.on("ReceiveQuestions", (questions) => {
+        console.log(questions);
+        quiz.value = questions;
     });
 
     connection.onclose(async () => {
@@ -39,6 +50,17 @@
 
 <template>
     <main>
-        Room
+        <h1>Room</h1>
+        <h2>Chat</h2>
+        <div class="chat" v-for="(message, index) in chat" :key="index">
+            <div>{{ message }}</div>
+        </div>
+        <h2>Quiz</h2>
+        <div class="quiz" v-for="question in quiz" :key="question.id">
+            <div v-if="question.difficulty">{{ question.difficulty }}</div>
+            <div v-if="question.category">{{ question.category }}</div>
+            <div v-if="question.title">{{ question.title }}</div>
+            <hr>
+        </div>
     </main>
 </template>
